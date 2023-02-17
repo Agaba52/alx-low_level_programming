b50cbff78d6ac80319d3d4028dfdfabfe21d7c94
Agaba_52 <agabaabel52@gmail.com>
2023-02-17 13:24:00 -0800

    programme to print size of variables in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{printf("size of char: %lu byt(s)/n", sizeof(char));
+printf("size of int: %lu byt(s)/n", sizeof(int));
+printf("size of long int: %lu byt(s)/n", sizeof(long int));
+printf("size of long  long int: %lu byt(s)/n", sizeof(long long int));
+printf("size of float: %lu byt(s)/n", sizeof(float));
+return (0);
+}
